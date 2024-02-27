97511bfbca14aafc2d0544e762e06fcfb8de87d2
yokotaerik <yokota082003@gmail.com>
2024-02-27 19:26:03 -0300

    Criando o projeto

---
+++ src/main/java/com/yokota/treino/TreinoApplication.java
@@ -0,0 +1,13 @@
+package com.yokota.treino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yokota/treino/TreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yokota.treino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
